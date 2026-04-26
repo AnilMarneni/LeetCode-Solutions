5dbb2a7519b23b5249eff2734d064fff5e31e92b
Anil Marneni <anilmarneni30@gmail.com>
2026-04-26 19:51:18 +0530

    Added: N-Queens (Backtracking)

---
+++ topics/backtracking/0051-n-queens.cpp
@@ -0,0 +1,52 @@
+/*
+Problem: N-Queens
+Link: https://leetcode.com/problems/n-queens/
+Difficulty: Hard
+
+Approach:
+- Use backtracking row by row
+- Place queen only if safe (no same column, diagonal)
+- Track columns and diagonals using sets/arrays
+
+Time Complexity: O(n!)
+Space Complexity: O(n)
+
+Tags: Backtracking
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> result;
+
+    void backtrack(int row, int n, vector<string>& board,
+                   vector<int>& col, vector<int>& diag1, vector<int>& diag2) {
+
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int c = 0; c < n; c++) {
+            if (col[c] || diag1[row + c] || diag2[row - c + n - 1]) continue;
+
+            board[row][c] = 'Q';
+            col[c] = diag1[row + c] = diag2[row - c + n - 1] = 1;
+
+            backtrack(row + 1, n, board, col, diag1, diag2);
+
+            board[row][c] = '.';
+            col[c] = diag1[row + c] = diag2[row - c + n - 1] = 0;
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        vector<int> col(n, 0), diag1(2 * n, 0), diag2(2 * n, 0);
+
+        backtrack(0, n, board, col, diag1, diag2);
+        return result;
+    }
+};
